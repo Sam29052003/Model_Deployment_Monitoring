23c13acb69b890c19336e44d91fd228b7e17fb30
S@muel_atkure <saatkure@gmail.com>
2026-01-03 13:58:58 +0530

    SUB TASK 6.2

---
+++ Sub Task 6.2/app.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+
+model = joblib.load("retail_model.joblib")
+
+app = FastAPI(
+    title="Retail Sales Prediction API",
+    description="Predict retail sales using ML model",
+    version="1.0"
+)
+
+class SalesInput(BaseModel):
+    customers: int
+    stores: int
+    products: int
+    discount: int
+    day: int
+    month: int
+
+@app.get("/")
+def home():
+    return {"message": "Retail Sales Prediction API is running"}
+
+@app.post("/predict")
+def predict_sales(data: SalesInput):
+
+    input_df = pd.DataFrame([{
+        "customers": data.customers,
+        "stores": data.stores,
+        "products": data.products,
+        "discount": data.discount,
+        "day": data.day,
+        "month": data.month
+    }])
+
+    prediction = model.predict(input_df)[0]
+
+    return {"predicted_sales": round(float(prediction), 2)}
